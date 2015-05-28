7add567c82110285973ffe7fb9ce2cb28d3b76ba
MiaHuang <l77324@gmail.com>
2015-05-28 20:49:57 +0800

    first release

---
+++ UseModelDAL/Program.cs
@@ -0,0 +1,26 @@
+﻿using MVCwithADO.NET.Models.DALs;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseModelDAL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductDal p = new ProductDal();
+            IEnumerable<DataRow> table = p.Get();
+            
+            foreach (System.Data.DataRow row in table)
+            {
+                Console.WriteLine("{0} : {1}",row["ProductID"],row["ProductName"]);
+            }
+
+            Console.Read();
+        }
+    }
+}
